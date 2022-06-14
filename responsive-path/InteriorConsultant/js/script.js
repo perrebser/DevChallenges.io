1d8a2e11d687a3ec0383ed0e14739a648c64ef5a
Sergio <101811829+perrebser@users.noreply.github.com>
2022-06-14 20:53:49 +0200

    Add files via upload

--- responsive-path/InteriorConsultant/js/script.js
+++ responsive-path/InteriorConsultant/js/script.js
@@ -1,11 +1,23 @@
-const menu_toggle = document.getElementById('cosas');
+const menu_toggle = document.getElementById('menu-icon');
 const menu_exit =  document.getElementById("close-btn");
 const primary_menu = document.getElementById("primary-navigation");
 
 menu_toggle.addEventListener("click", () => {
     const visibility = primary_menu.getAttribute('data-visible');
-    console.log(visibility);
+    if (visibility =="false") {
+       primary_menu.setAttribute('data-visible',"true");
+       menu_exit.setAttribute('data-visible',"true");
+       console.log(visibility);
+    } else {
+        primary_menu.setAttribute('data-visible',"false");
+        menu_exit.setAttribute('data-visible',"false");
+    }
+})
 
-
-
+menu_exit.addEventListener("click", () =>  {
+    const visibility = menu_exit.getAttribute('data-visible');
+    if (visibility=="true") {
+        primary_menu.setAttribute('data-visible',"false");
+        menu_exit.setAttribute('data-visible',"false");
+    }
 })
